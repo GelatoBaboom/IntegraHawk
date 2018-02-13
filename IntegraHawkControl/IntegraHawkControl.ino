69361c1a7daac9522bef895f977f28e6b7591241
Ignacio <idx001@gmail.com>
2018-02-13 13:42:36 -0300

    con trimer de posicion y

--- IntegraHawkControl/IntegraHawkControl.ino
+++ IntegraHawkControl/IntegraHawkControl.ino
@@ -2,9 +2,12 @@
 #include "Antenna.h"
 const int analogPinX = A0;
 const int analogPinY = A1;
+const int analogPinYCorrection = A3;
 const int analogPinESC = A2;
+
 int valueX = 0;
 int valueY = 0;
+int valueYCorr = 0;
 int valueESC = 0;
 //para pruebas 
 uint32_t timer;
@@ -19,6 +22,7 @@ void setup() {
 void loop() {
 	valueX = analogRead(analogPinX);
 	valueY = analogRead(analogPinY);
+	valueYCorr = analogRead(analogPinYCorrection);
 	valueESC = analogRead(analogPinESC);
 
 	/*if (((micros() - timer) / 1000) > 2000)
@@ -35,7 +39,9 @@ void loop() {
 	//antSerial.println(a.toString());
 
 	int actPositionx = map(valueX, 0, 1023, 126, -126);
-	int actPositiony = map(valueY, 0, 1023, -126, 126);
+	int actPositionycorr = map(valueYCorr, 0, 1023, -90, 90);
+	//Serial.println(String( actPositionycorr));
+	int actPositiony = map((valueY + actPositionycorr), 0, 1023, -126, 126);
 	int actPositionESC = map(valueESC, 0, 1023, 126, -126);
 	byte buffer[5] = { (char)-127,(char)actPositionx, (char)actPositiony,(char)actPositionESC,(char)127 };
 	
